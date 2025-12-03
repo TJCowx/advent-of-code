afe6459ef547a58b26dd87f7cbe455332952380c
TJCowx21 <tcowx21@gmail.com>
2025-12-03 00:02:38 -0500

    Day 2 boilerplate

---
+++ 2025/day02/day02.go
@@ -0,0 +1,24 @@
+package day02
+
+import (
+	"advent-of-code/go_utils"
+	"fmt"
+)
+
+// https://adventofcode.com/2025/day/2
+
+func Run(part *string) {
+	go_utils.RunParts(part, "day02/input.txt", part1, part2)
+}
+
+func part1(path string) int {
+	fmt.Println("Day 02, Part 1: START")
+
+	return 0
+}
+
+func part2(path string) int {
+	fmt.Println("Day 02, Part 2: START")
+
+	return 0
+}
