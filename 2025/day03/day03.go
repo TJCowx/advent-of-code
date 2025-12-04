cc0e00d651193eec09b6a3e382e26248d0ab50c7
TJCowx21 <tcowx21@gmail.com>
2025-12-03 21:41:13 -0500

    day 3 part 2 bruteforce checkpoint

--- 2025/day03/day03.go
+++ 2025/day03/day03.go
@@ -53,12 +53,85 @@ func part1(path string) int {
 	return result
 }
 
+func getHighestStarts(bank []int) (int, []int) {
+	highestStart, matchedIs := 0, []int{}
+	fmt.Println(bank)
+	for i := 0; i < len(bank)-12; i++ {
+		val := bank[i]
+
+		if val > highestStart {
+			highestStart = val
+			matchedIs = []int{i}
+		} else if val == highestStart {
+			matchedIs = append(matchedIs, i)
+		}
+	}
+
+	return highestStart, matchedIs
+}
+
+func getHighestCombo(bank []int, currI int, currCombo []int, bankSize int) int {
+	combo := append(currCombo, bank[currI])
+	if len(combo) == 12 {
+		res, err := go_utils.CombineSimpleInts(combo)
+
+		if err != nil {
+			log.Fatalf("Invalid combo (%d) %s", currCombo, err)
+		}
+
+		return res
+	}
+
+	// Size guard, if we need more numbers than we have left, lets just leave it
+	remaining := 12 - len(combo)
+	if remaining > (bankSize - currI) {
+		return -1
+	}
+
+	highestCombo := 0
+
+	// BANK: 811111111111119
+	// EXPECTED: 811111111119
+	// GOT: 81111111111
+	for i := currI + 1; i < bankSize; i++ {
+		comboVal := getHighestCombo(bank, i, combo, bankSize)
+		if comboVal > highestCombo {
+			highestCombo = comboVal
+		}
+	}
+
+	return highestCombo
+}
+
 func part2(path string) int {
 	fmt.Println("Day 03, Part 2: START")
 	result := 0
 
+	banks, err := go_utils.ReadIntoStrArr(path)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	timer := go_utils.Timer{}
 	timer.Start()
+
+	for _, bankStr := range banks {
+		bank := go_utils.StringAsNumArray(bankStr)
+		highestStart, matchedIs := getHighestStarts(bank)
+
+		highestCombo := 0
+
+		for _, matchedI := range matchedIs {
+			combo := getHighestCombo(bank, matchedI, []int{}, len(bank))
+			if combo > highestCombo {
+				highestCombo = combo
+			}
+		}
+
+		fmt.Printf("Highest: %d | Indexes: %v | Highest combo: %d\n", highestStart, matchedIs, highestCombo)
+		result += highestCombo
+	}
 
 	timer.End()
 	fmt.Printf("day 03, part 1 result: %d | %s\n", result, timer.TimeLapsed())
