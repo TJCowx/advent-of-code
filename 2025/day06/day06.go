a32e65e0822481b306276630198da213fc847e63
TJCowx21 <tcowx21@gmail.com>
2025-12-06 11:57:59 -0500

    Day 6 part 2

--- 2025/day06/day06.go
+++ 2025/day06/day06.go
@@ -19,6 +19,12 @@ type mathProblem struct {
 	deliminator string
 }
 
+type column struct {
+	startI      int
+	endI        int
+	deliminator string
+}
+
 func parseInput(rows []string) []mathProblem {
 	problems := make([]mathProblem, len(strings.Fields(rows[0])))
 
@@ -75,17 +81,91 @@ func part1(path string) int {
 	return result
 }
 
+// All deliminators are left aligned to the problem so we know the start of each problem
+// And if there is 1 space between each column, the end of it is 2 spaces back from current
+func findOperators(delimRow string) []int {
+	var indexes []int
+
+	for i, r := range delimRow {
+		if r == '+' || r == '*' {
+			indexes = append(indexes, i)
+		}
+	}
+
+	return indexes
+}
+
+func parseInputP2(rows []string) []mathProblem {
+	var problems []mathProblem
+
+	operatorIndexes := findOperators(rows[len(rows)-1])
+
+	// Start going right to left
+	for i := len(operatorIndexes) - 1; i >= 0; i-- {
+		currOpI := operatorIndexes[i]
+		var colLastI int
+		// Guard against last op i, the end of the column would be the length
+		if i == len(operatorIndexes)-1 {
+			colLastI = len(rows[0]) - 1
+		} else {
+			// The last column would be 2 previous from the next operator
+			// But the last colI is exclusive
+			colLastI = operatorIndexes[i+1] - 2
+		}
+
+		problem := mathProblem{
+			values:      []int{},
+			deliminator: string(rows[len(rows)-1][currOpI]),
+		}
+
+		// Iterate through the columns
+		for colI := colLastI; colI >= currOpI; colI-- {
+			// Iterate through the columns, not including last and build the string
+			// Ignoring the last I of the row as it is the deliminator
+			var built string
+			for rowI := 0; rowI < len(rows)-1; rowI++ {
+				charVal := string(rows[rowI][colI])
+				if charVal != " " {
+					built += charVal
+				}
+			}
+			newVal, err := strconv.Atoi(built)
+
+			if err != nil {
+				log.Fatalf("Error converting (%s): %s\n", built, err)
+			}
+
+			problem.values = append(problem.values, newVal)
+		}
+
+		problems = append(problems, problem)
+	}
+
+	return problems
+}
+
 func part2(path string) int {
 	fmt.Println("Day 06, Part 2: START")
 	result := 0
 
 	timer := go_utils.Timer{}
+	rows, err := go_utils.ReadIntoStrArr(path)
+
+	if err != nil {
+		log.Fatalf("Error reading input: %s", err)
+	}
 
 	timer.Start()
 
+	problems := parseInputP2(rows)
+
+	for _, p := range problems {
+		result += solveProblem(p)
+	}
+
 	timer.End()
 
-	fmt.Printf("day 06, part 1 result: %d | %s\n", result, timer.TimeLapsed())
+	fmt.Printf("day 06, part 2 result: %d | %s\n", result, timer.TimeLapsed())
 
 	return result
 }
